f2c0af356ebe96e9e46cfc5bb038ab9296321884
Nestor Gonzalez <nesgonsua@gmail.com>
2024-04-15 17:47:29 +0100

    feat: test Genero

--- scr/statics/Genero.java
+++ scr/statics/Genero.java
@@ -2,5 +2,27 @@ package scr.statics;
 
 
 public enum Genero {
-    
+    ACCION(1),
+    AVENTURA(2),
+    COMEDIA(3),
+    DRAMA(4),
+    FANTASIA(5),
+    TERROR(6),
+    CIENCIA_FICCION(7),
+    MUSICAL_DANZA(8),
+    SUSPENSE(9),
+    WESTERN(10),
+    DOCUMENTAL(11),
+    BIOGRAFICO(12),
+    ROMANCE(13);
+
+    private final int id;
+
+    Genero(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
--- test/TestStatics.java
+++ test/TestStatics.java
@@ -1,5 +1,41 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import scr.statics.Genero;
+
 public class TestStatics {
+
+    @Test
+    public void testGenero() {
+        Genero accion = Genero.ACCION;
+        Genero aventura = Genero.AVENTURA;
+        Genero comedia = Genero.COMEDIA;
+        Genero drama = Genero.DRAMA;
+        Genero fantasia = Genero.FANTASIA;
+        Genero terror = Genero.TERROR;
+        Genero cienciaFiccion = Genero.CIENCIA_FICCION;
+        Genero musicalDanza = Genero.MUSICAL_DANZA;
+        Genero suspense = Genero.SUSPENSE;
+        Genero western = Genero.WESTERN;
+        Genero documental = Genero.DOCUMENTAL;
+        Genero biografico = Genero.BIOGRAFICO;
+        Genero romance = Genero.ROMANCE;
+
+        assertEquals(accion.getId(), 1);
+        assertEquals(aventura.getId(), 2);
+        assertEquals(comedia.getId(), 3);
+        assertEquals(drama.getId(), 4);
+        assertEquals(fantasia.getId(), 5);
+        assertEquals(terror.getId(), 6);
+        assertEquals(cienciaFiccion.getId(), 7);
+        assertEquals(musicalDanza.getId(), 8);
+        assertEquals(suspense.getId(), 9);
+        assertEquals(western.getId(), 10);
+        assertEquals(documental.getId(), 11);
+        assertEquals(biografico.getId(), 12);
+        assertEquals(romance.getId(), 13);
+    }
     
 }
